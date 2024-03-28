0d45ee5f2e67ccf2f51d79a3d715c231adccaf92
Chris Wood <wchristopherwood912@gmail.com>
2024-03-27 19:50:25 -0700

    update formatting

--- makesite.go
+++ makesite.go
@@ -8,44 +8,44 @@ import (
 )
 
 type Page struct {
-	TextFilePath string
-	TextFileName string
-	HTMLPagePath string
-	Content string
+        TextFilePath string
+        TextFileName string
+        HTMLPagePath string
+        Content      string
 }
 
 func main() {
-	// read input
-	textFilePathPtr := flag.String("file", "new-post.txt", "path of .txt file in the current directory.")
-	flag.Parse()
-	textFileName := strings.Trim(*textFilePathPtr, ".txt")
+        // read input
+        textFilePathPtr := flag.String("file", "first-post.txt", "path of .txt file in the current directory.")
+        flag.Parse()
+        textFileName := strings.Trim(*textFilePathPtr, ".txt")
 
-	fileContents, err := os.ReadFile(*textFilePathPtr)
+        fileContents, err := os.ReadFile(*textFilePathPtr)
 
-	if err!= nil {
-		panic(err)
-	}
+        if err != nil {
+                panic(err)
+        }
 
-	//create page struct
-	page := Page{
-		TextFilePath: *textFilePathPtr,
-		TextFileName: textFileName,
-		HTMLPagePath: textFileName+".html",
-		Content: string(fileContents),
-	}
+        //create page struct
+        page := Page{
+                TextFilePath: *textFilePathPtr,
+                TextFileName: textFileName,
+                HTMLPagePath: textFileName + ".html",
+                Content:      string(fileContents),
+        }
 
-	// Create template
-	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+        // Create template
+        t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 
-	newFile, err := os.Create(page.HTMLPagePath)
-	if err != nil {
-		panic(err)
-	}
+        newFile, err := os.Create(page.HTMLPagePath)
+        if err != nil {
+                panic(err)
+        }
 
-	//generate html
-	err = t.Execute(newFile, page)
-	if err != nil {
-		panic(err)
-	}
-	
+        //generate html
+        err = t.Execute(newFile, page)
+        if err != nil {
+                panic(err)
+        }
+
 }
