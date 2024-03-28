30daa953b30dd99e9f40388835fc855806a4bd03
Chris Wood <wchristopherwood912@gmail.com>
2024-03-27 18:21:08 -0700

    render template and output to console

--- makesite.go
+++ makesite.go
@@ -1,15 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"html/template"
+	"os"
 )
 
+type Page struct {
+	TextFilePath string
+	TextFileName string
+	HTMLPagePath string
+	Content string
+}
+
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	// read input
+	textFilePath := "first-post.txt"
+	textFileName := "new"
+
+	fileContents, err := os.ReadFile(textFilePath)
+
 	if err!= nil {
 		panic(err)
 	}
 
-	fmt.Print(string(fileContents))
+	//create page struct
+	page := Page{
+		TextFilePath: textFilePath,
+		TextFileName: textFileName,
+		HTMLPagePath: textFileName+".html",
+		Content: string(fileContents),
+	}
+
+	// Create template
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	// newFile, err := os.Create(page.HTMLPagePath)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	//generate html
+	err = t.Execute(os.Stdout, page)
+	if err != nil {
+		panic(err)
+	}
+	
 }
