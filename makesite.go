8cc88368ba1a774a12e20fc8cef63dc98510e43f
Chris Wood <wchristopherwood912@gmail.com>
2024-04-04 12:43:44 -0700

    v1.1 done

--- makesite.go
+++ makesite.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"flag"
-	"html/template"
+	"fmt"
 	"os"
 	"strings"
+	"text/template"
 )
 
 type Page struct {
@@ -14,24 +15,32 @@ type Page struct {
 	Content string
 }
 
-func main() {
-	// read input
-	textFilePathPtr := flag.String("file", "first-post.txt", "path of .txt file in the current directory.")
-	flag.Parse()
-	textFileName := strings.Trim(*textFilePathPtr, ".txt")
-
-	fileContents, err := os.ReadFile(*textFilePathPtr)
-
-	if err!= nil {
+func readDir (path string) ([]string, error) {
+	files, err := os.ReadDir(path)
+	txtFiles := []string{}
+	if err != nil {
 		panic(err)
 	}
 
+	for _, file := range(files) {
+		_, path, isCut:= strings.Cut(file.Name(), ".")
+		if isCut {
+			if path == "txt" {
+				txtFiles = append(txtFiles, file.Name())
+			}
+		}
+	}
+
+	return txtFiles, nil
+}
+
+func createPage(filePath, fileName, Contents string) (error) {
 	//create page struct
 	page := Page{
-		TextFilePath: *textFilePathPtr,
-		TextFileName: textFileName,
-		HTMLPagePath: textFileName+".html",
-		Content: string(fileContents),
+		TextFilePath: filePath,
+		TextFileName: fileName,
+		HTMLPagePath: fileName+".html",
+		Content: Contents,
 	}
 
 	// Create template
@@ -39,13 +48,57 @@ func main() {
 
 	newFile, err := os.Create(page.HTMLPagePath)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	//generate html
 	err = t.Execute(newFile, page)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	fmt.Println("done")
+
+	return nil
+} 
+
+func main() {
+	// read input
+	textFilePathPtr := flag.String("file", "", "path of .txt file in the current directory.")
+	textFileDirPtr := flag.String("dir", "./", "path to text files for static site generation")
+	flag.Parse()	
+	
+	if *textFilePathPtr != "" {
+		textFileName := strings.Trim(*textFilePathPtr, ".txt")
+		fileContents, err := os.ReadFile(*textFilePathPtr)
+
+		if err!= nil {
+			panic(err)
+		}
+
+		createPage(*textFilePathPtr, textFileName, string(fileContents))
+
+		
+	} else {
+		files, err := readDir(*textFileDirPtr)
+		if err != nil {
+			panic(err)
+		}
+		
+		for _, file := range(files) {
+			path := *textFileDirPtr + "/" + file
+			fileContents, err := os.ReadFile(path)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(file + " htmling")
+			err = createPage(path, strings.Trim(path, ".txt"), string(fileContents))
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	
 	
 }
